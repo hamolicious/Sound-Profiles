d5b22fd76d896cf8d8ba799e84d80aa3f21c9ca4
hamolicious <kickfliptv8@gmail.com>
2022-01-31 04:59:00 +0000

    added scaling GUI

--- src/main/java/hamolicious/sound/profiles/client/SettingsClientGUI.java
+++ src/main/java/hamolicious/sound/profiles/client/SettingsClientGUI.java
@@ -9,27 +9,48 @@ import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
 import io.github.cottonmc.cotton.gui.widget.WScrollPanel;
 import io.github.cottonmc.cotton.gui.widget.data.Insets;
 import net.fabricmc.fabric.api.util.TriState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Window;
 import net.minecraft.text.TranslatableText;
 
 public class SettingsClientGUI extends LightweightGuiDescription {
+	private static final float ROOT_PANEL_WIDTH_MULTIPLIER = 0.5f;
+	private static final float ROOT_PANEL_HEIGHT_MULTIPLIER = 0.5f;
+
+	private void setupButtons(WPlainPanel root, int width, int height) {
+		WButton[] buttons = new WButton[1];
+
+		// Save State Button
+		WButton buttonSaveCurrentState = new WButton(
+				new TranslatableText("button.save.state.hamolicious.sound.profiles"));
+		buttonSaveCurrentState.setOnClick(() -> {
+			SoundProfile soundProfile = new SoundProfile();
+			soundProfile.saveCurrentSettings();
+			SoundProfiles.profiles.add(soundProfile);
+		});
+		buttons[0] = buttonSaveCurrentState;
+
+		for (int i = 0; i < buttons.length; i++) {
+			int w = (int)(((float)width) / buttons.length + 5);
+			int x = i * w;
+			root.add(buttons[i], x, 20, w, 40);
+		}
+	}
+
 	public SettingsClientGUI() {
 		WPlainPanel root = new WPlainPanel();
 		setRootPanel(root);
-		root.setSize(256, 240);
+		Window win = MinecraftClient.getInstance().getWindow();
+		final int WIDTH = (int)((float)win.getScaledWidth() * ROOT_PANEL_WIDTH_MULTIPLIER);
+		final int HEIGHT = (int)((float)win.getScaledHeight() * ROOT_PANEL_HEIGHT_MULTIPLIER);
+		root.setSize(WIDTH, HEIGHT);
 		root.setInsets(Insets.ROOT_PANEL);
 
 		// Title
 		WLabel label = new WLabel(new TranslatableText("category.hamolicious.sound.profiles"));
 		root.add(label, 0, 0);
 
-		// Save State Button
-		WButton buttonSaveCurrentState = new WButton(new TranslatableText("button.save.state.hamolicious.sound.profiles"));
-		root.add(buttonSaveCurrentState, 0, 40, 256, 40);
-		buttonSaveCurrentState.setOnClick(() -> {
-			SoundProfile soundProfile = new SoundProfile();
-			soundProfile.saveCurrentSettings();
-			SoundProfiles.profiles.add(soundProfile);
-		});
+		setupButtons(root, WIDTH, HEIGHT);
 
 		// Create Sound Profiles Panel
 		WPlainPanel plainPanel = new WPlainPanel();
@@ -40,11 +61,9 @@ public class SettingsClientGUI extends LightweightGuiDescription {
 				soundProfile.applyProfile();
 			});
 
-
 			label = new WLabel(soundProfile.getName());
-			//TODO: add automatic scaling for all GUI elements
-			plainPanel.add(button, 256/2, i*45, 256/2, 40);
-			plainPanel.add(label, 0, i*45, 256/2, 40);
+			plainPanel.add(button, WIDTH / 2, i * 45, WIDTH / 2, 40);
+			plainPanel.add(label, 0, i * 45, WIDTH / 2, 40);
 		}
 
 		// Add above panel to scroll panel to allow scrolling
@@ -52,7 +71,7 @@ public class SettingsClientGUI extends LightweightGuiDescription {
 		scrollPanel.setScrollingHorizontally(TriState.FALSE);
 		scrollPanel.setScrollingVertically(TriState.DEFAULT);
 
-		root.add(scrollPanel, 0, 80, 256, 240-80);
+		root.add(scrollPanel, 0, 80, WIDTH, 240 - 80);
 		root.validate(this);
 	}
 }
