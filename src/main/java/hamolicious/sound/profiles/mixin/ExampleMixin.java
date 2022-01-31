958a9a2cd067a2731eda26d23d14ab3503f0ed92
hamolicious <kickfliptv8@gmail.com>
2022-01-31 01:16:24 +0000

    setup mod options and prequisites

--- src/main/java/hamolicious/sound/profiles/mixin/ExampleMixin.java
+++ src/main/java/hamolicious/sound/profiles/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package hamolicious.sound.profiles.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import hamolicious.sound.profiles.SoundProfiles;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		SoundProfiles.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
