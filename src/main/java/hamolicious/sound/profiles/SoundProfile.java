c129330c8997ae8c04374cf1a2b72cd3bd532b9a
hamolicious <kickfliptv8@gmail.com>
2022-01-31 02:02:16 +0000

    created concept

---
+++ src/main/java/hamolicious/sound/profiles/SoundProfile.java
@@ -0,0 +1,71 @@
+package hamolicious.sound.profiles;
+
+import java.util.HashMap;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.sound.SoundCategory;
+
+
+
+public class SoundProfile {
+	private HashMap<SoundCategory, Float> soundLevels;
+
+	SoundProfile() {
+		soundLevels = new HashMap<SoundCategory, Float>();
+		soundLevels.put(SoundCategory.MASTER, 1.0f);
+		soundLevels.put(SoundCategory.MUSIC, 1.0f);
+		soundLevels.put(SoundCategory.RECORDS, 1.0f);
+		soundLevels.put(SoundCategory.WEATHER, 1.0f);
+		soundLevels.put(SoundCategory.BLOCKS, 1.0f);
+		soundLevels.put(SoundCategory.HOSTILE, 1.0f);
+		soundLevels.put(SoundCategory.NEUTRAL, 1.0f);
+		soundLevels.put(SoundCategory.PLAYERS, 1.0f);
+		soundLevels.put(SoundCategory.AMBIENT, 1.0f);
+		soundLevels.put(SoundCategory.VOICE, 1.0f);
+	}
+
+	public void saveCurrentSettings() {
+		MinecraftClient client = MinecraftClient.getInstance();
+		try { // incase something goes wrong...
+			GameOptions options = client.options;
+			soundLevels.put(SoundCategory.MASTER, options.getSoundVolume(SoundCategory.MASTER));
+			soundLevels.put(SoundCategory.MUSIC, options.getSoundVolume(SoundCategory.MUSIC));
+			soundLevels.put(SoundCategory.RECORDS, options.getSoundVolume(SoundCategory.RECORDS));
+			soundLevels.put(SoundCategory.WEATHER, options.getSoundVolume(SoundCategory.WEATHER));
+			soundLevels.put(SoundCategory.BLOCKS, options.getSoundVolume(SoundCategory.BLOCKS));
+			soundLevels.put(SoundCategory.HOSTILE, options.getSoundVolume(SoundCategory.HOSTILE));
+			soundLevels.put(SoundCategory.NEUTRAL, options.getSoundVolume(SoundCategory.NEUTRAL));
+			soundLevels.put(SoundCategory.PLAYERS, options.getSoundVolume(SoundCategory.PLAYERS));
+			soundLevels.put(SoundCategory.AMBIENT, options.getSoundVolume(SoundCategory.AMBIENT));
+			soundLevels.put(SoundCategory.VOICE, options.getSoundVolume(SoundCategory.VOICE));
+		} catch (Exception e) { // ...wanna get rid of the client
+			client.close();
+			e.printStackTrace();
+		}
+	}
+
+	public void setSoundLevel(SoundCategory soundType, float level) {
+		soundLevels.put(soundType, level);
+	}
+
+	public void applyProfile() {
+		MinecraftClient client = MinecraftClient.getInstance();
+		try { // incase something goes wrong...
+			GameOptions options = client.options;
+			options.setSoundVolume(SoundCategory.MASTER, (float)soundLevels.get(SoundCategory.MASTER));
+			options.setSoundVolume(SoundCategory.MUSIC, (float)soundLevels.get(SoundCategory.MUSIC));
+			options.setSoundVolume(SoundCategory.RECORDS, (float)soundLevels.get(SoundCategory.RECORDS));
+			options.setSoundVolume(SoundCategory.WEATHER, (float)soundLevels.get(SoundCategory.WEATHER));
+			options.setSoundVolume(SoundCategory.BLOCKS, (float)soundLevels.get(SoundCategory.BLOCKS));
+			options.setSoundVolume(SoundCategory.HOSTILE, (float)soundLevels.get(SoundCategory.HOSTILE));
+			options.setSoundVolume(SoundCategory.NEUTRAL, (float)soundLevels.get(SoundCategory.NEUTRAL));
+			options.setSoundVolume(SoundCategory.PLAYERS, (float)soundLevels.get(SoundCategory.PLAYERS));
+			options.setSoundVolume(SoundCategory.AMBIENT, (float)soundLevels.get(SoundCategory.AMBIENT));
+			options.setSoundVolume(SoundCategory.VOICE, (float)soundLevels.get(SoundCategory.VOICE));
+		} catch (Exception e) { // ...wanna get rid of the client
+			client.close();
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/hamolicious/sound/profiles/SoundProfiles.java
+++ src/main/java/hamolicious/sound/profiles/SoundProfiles.java
@@ -1,6 +1,13 @@
 package hamolicious.sound.profiles;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
+
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +17,79 @@ public class SoundProfiles implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("soundprofiles");
 
+	private static KeyBinding setSoundProfile1;
+	private static KeyBinding setSoundProfile2;
+	private static KeyBinding saveSoundProfile1;
+	private static KeyBinding saveSoundProfile2;
+
+	private static SoundProfile prof1;
+	private static SoundProfile prof2;
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		prof1 = new SoundProfile();
+		prof2 = new SoundProfile();
+
+		setSoundProfile1 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.apply.profile.one.hamolicious.sound.profiles", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_R, // The keycode of the key
+				"category.hamolicious.sound.profiles" // The translation key of the keybinding's category.
+		));
+
+		setSoundProfile2 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.apply.profile.two.hamolicious.sound.profiles", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_R, // The keycode of the key
+				"category.hamolicious.sound.profiles" // The translation key of the keybinding's category.
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (setSoundProfile1.wasPressed()) {
+				client.player.sendMessage(new LiteralText("Loading Profile 1!"), false);
+				prof1.applyProfile();
+			}
+		});
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (setSoundProfile2.wasPressed()) {
+				client.player.sendMessage(new LiteralText("Loading Profile 2!"), false);
+				prof2.applyProfile();
+			}
+		});
+
+
+		saveSoundProfile1 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.save.profile.one.hamolicious.sound.profiles", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_K, // The keycode of the key
+				"category.hamolicious.sound.profiles" // The translation key of the keybinding's category.
+		));
+
+		saveSoundProfile2 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.save.profile.two.hamolicious.sound.profiles", // The translation key of the keybinding's name
+				InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_L, // The keycode of the key
+				"category.hamolicious.sound.profiles" // The translation key of the keybinding's category.
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (saveSoundProfile1.wasPressed()) {
+				client.player.sendMessage(new LiteralText("Saving Profile 1!"), false);
+				prof1.saveCurrentSettings();
+			}
+		});
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (saveSoundProfile2.wasPressed()) {
+				client.player.sendMessage(new LiteralText("Saving Profile 2!"), false);
+				prof2.saveCurrentSettings();
+			}
+		});
+
 	}
 }
